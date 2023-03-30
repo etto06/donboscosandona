a5c40288af141e1c52aab20044dc6404a8404f6f
Ettore Canever <etto06etto@gmail.com>
2023-03-30 11:54:23 +0200

    creazione del footer vuoto

---
+++ dev/ettore/Footer.js
@@ -0,0 +1,5 @@
+export default function Footer(props){
+    return(
+        <h1>footer</h1>
+    )
+}
---
+++ pages/dev/ettore/index.js
@@ -0,0 +1,7 @@
+import Layout from "@/components/Layout";
+import Footer from "@/dev/ettore/Footer";
+export default function Page(){
+    return(
+        <Footer></Footer>
+    )
+}
